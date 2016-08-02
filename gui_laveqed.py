280fec3144a51736c9f25eb2935fd5ff8e014ab6
JeanOlivier <jean.olivier.simoneau@usherbrooke.ca>
2016-08-02 15:27:43 -0400

    Less initial support to scale SVG images

--- gui_laveqed.py
+++ gui_laveqed.py
@@ -435,13 +435,13 @@ class laveqed_gui(object):
         # Based on pygame.org/wiki/CairoPygame and http://bit.ly/1hnpYZY        
         svg = rsvg.Handle(file=file_path_name)
         width, height = svg.get_dimension_data()[:2]
-        surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, int(width), int(height))
+        surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, int(width*scale), int(height*scale))
         context = cairo.Context(surface)
         context.scale(scale,scale)
         #context.set_antialias(cairo.ANTIALIAS_SUBPIXEL)
         svg.render_cairo(context)
         tk_image=ImageTk.PhotoImage('RGBA')
-        image=Image.frombuffer('RGBA',(width,height),surface.get_data(),'raw','BGRA',0,1)
+        image=Image.frombuffer('RGBA',(int(width*scale),int(height*scale)),surface.get_data(),'raw','BGRA',0,1)
         tk_image.paste(image)
         return(tk_image)
 
